971dbe135aa64221f4e408c06cb82dd5a8a63ac5
Ole Bjørn Michelsen <ole@michelsen.dk>
2015-03-13 14:11:52 +0100

    Added test for new getVariant middleware.

---
+++ test/getVariant.js
@@ -0,0 +1,33 @@
+var ab = require('../lib/express-ab');
+var assert = require('assert');
+var cookieParser = require('cookie-parser');
+var express = require('express');
+var request = require('supertest');
+
+describe('getVariant', function () {
+    var app = express();
+    app.use(cookieParser());
+
+    var abTest = ab.test('unit-test');
+
+    app.get('/', abTest.getVariant, function (req, res) {
+        if (res.locals.ab) {
+            res.send('variant' + res.locals.ab.variantId);
+        } else {
+            res.send('no cookie');
+        }
+    });
+
+    it('should read cookie variant from res.locals.ab', function (done) {
+        request(app)
+            .get('/')
+            .set('Cookie', ['ab=%7B%22unit-test%22%3A42%7D'])
+            .expect('variant42', done);
+    });
+
+    it('should continue if no cookie is set', function (done) {
+        request(app)
+            .get('/')
+            .expect('no cookie', done);
+    });
+});
