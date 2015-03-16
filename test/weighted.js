1078c309bab7b4d0047c7ba7c376bc802120be6f
Ole Bjørn Michelsen <ole@michelsen.dk>
2015-03-16 16:55:45 +0100

    Added test to ensure that first part of weighted route can be skipped (e.g. browser checks).

--- test/weighted.js
+++ test/weighted.js
@@ -4,9 +4,6 @@ var express = require('express');
 var request = require('supertest');
 
 describe('weighted', function () {
-    var app = express();
-    var abTest = ab.test('unit-test');
-
     function setReqVarMiddleware(req, res, next) {
         req.ab = {
             random: req.get('ab-random'),
@@ -14,6 +11,13 @@ describe('weighted', function () {
         };
         next();
     }
+
+    function skipRouteMiddleware(req, res, next) {
+        next('route');
+    }
+
+    var app = express();
+    var abTest = ab.test('unit-test');
 
     app.get('/', setReqVarMiddleware, abTest(null, 0.2), function (req, res) {
         res.send('variantA');
@@ -49,6 +53,25 @@ describe('weighted', function () {
         it('should select route B', function (done) {
             request(app)
                 .get('/')
+                .set('ab-random', 0.42)
+                .expect(200)
+                .expect('variantB', done);
+        });
+    });
+
+    describe('fallthrough', function () {
+
+        app.get('/fallthrough', skipRouteMiddleware, setReqVarMiddleware, abTest(null, 0.5), function (req, res) {
+            res.send('variantA');
+        });
+
+        app.get('/fallthrough', setReqVarMiddleware, abTest(null, 0.5), function (req, res) {
+            res.send('variantB');
+        });
+
+        it('should fallthrough if first is skipped', function (done) {
+            request(app)
+                .get('/fallthrough')
                 .set('ab-random', 0.42)
                 .expect(200)
                 .expect('variantB', done);
